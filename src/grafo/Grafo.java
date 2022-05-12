e92bfa09a0b090393f94d36f157ee024095c6e6b
GonRodCa <gonzalo-r@hotmail.es>
2022-05-11 22:20:41 -0300

    a

--- src/grafo/Grafo.java
+++ src/grafo/Grafo.java
@@ -24,17 +24,27 @@ public class Grafo {
     }
 
     public boolean esLleno() {
-        //Implementar...
-        return false;
+        for(int i = 0; i< vertices.length;i++){
+            if(vertices[i] == null){ return false; }
+        }
+        return true;
     }
 
     public boolean esVacio() {
         //Implementar...
-        return false;
+        for(int i = 0; i< vertices.length;i++){
+            if(vertices[i] != null){ return false; }
+        }
+        return true;
+
+
     }
 
     // PRE: !esLleno()
     private int obtenerPosLibre() {
+        for(int i = 0; i <vertices.length;i++){
+            if(vertices[i] == null) { return i; }
+        }
         //Implementar...
         return -1;
     }
@@ -52,15 +62,31 @@ public class Grafo {
     // PRE: !esLleno && !existeVertice
     public void agregarVertice(String vert) {
         //Implementar...
+        for(int i = 0; i < vertices.length; i++){
+            if(vertices[i] == null){
+                vertices[i] = vert;
+                break;
+            }
+        }
     }
 
     // PRE: existeVertice
     public void borrarVertice(String vert) {
+        for(int i = 0; i < vertices.length; i ++){
+            if(vertices[i] == vert) {
+                vertices[i] = null;
+            }
+        }
         //Implementar...
     }
 
     public boolean existeVertice(String vert) {
         //Implementar...
+        for(int i = 0; i < vertices.length; i ++){
+            if(vertices[i] == vert) {
+                return true;
+            }
+        }
         return false;
     }
 
@@ -68,21 +94,40 @@ public class Grafo {
     public void agregarArista(String origen, String destino, int peso) {
         //Implementar...
 
+        matAdy[obtenerPos(origen)][obtenerPos(destino)].setPeso(peso);
+        matAdy[obtenerPos(origen)][obtenerPos(destino)].setExiste(true);
+
+
+
     }
 
     // existeVertice(origen) && existeVertice(destino)
     public boolean existerArista(String origen, String destino) {
-        //Implementar...
-        return false;
+        int posO = obtenerPos(origen);
+        int posD = obtenerPos(destino);
+
+        return matAdy[posO][posD].isExiste() || matAdy[posD][posO].isExiste();
+
     }
+
 
     // existeVertice(origen) && existeVertice(destino) && existeArista
     public void borrarArista(String origen, String destino) {
         //Implementar...
+        int posO = obtenerPos(origen);
+        int posD = obtenerPos(destino);
+        matAdy[posO][posD].setExiste(false);
+        matAdy[posO][posD].setPeso(0);
     }
 
     public Lista<String> verticesAdyacentes(String vert) {
         Lista<String> retorno = new ListaImp<>();
+        int ubiV = obtenerPos(vert);
+        for(int i = 0; i<vertices.length; i++){
+            if(matAdy[ubiV][i].isExiste()){
+                retorno.insertar(vertices[i]);
+            }
+        }
         //Implementar...
         return retorno;
     }
@@ -90,6 +135,12 @@ public class Grafo {
     // Pre: existeVertice(vert)
     public Lista<String> verticesIncidentes(String vert) {
         Lista<String> retorno = new ListaImp<>();
+        int ubi = obtenerPos(vert);
+        for(int i = 0; i < vertices.length; i++){
+            if(matAdy[i][ubi].isExiste()){
+                retorno.insertar(vertices[i]);
+            }
+        }
         //Implementar...
         return retorno;
     }
---
+++ src/grafo/main.java
@@ -0,0 +1,9 @@
+package grafo;
+
+public class main {
+    public static void main(String[] args) {
+        String[] s = new String[5];
+        s[0] = "a";
+        System.out.println(s[0]);
+    }
+}
